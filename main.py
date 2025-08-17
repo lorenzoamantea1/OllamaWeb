b0b38d481516ea28c1e2eef030502db830eb3f81
Lorenzo Amantea <lorenzo.amantea1@gmail.com>
2025-08-17 04:52:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
 from flask import Flask, render_template
 from core.routes import chat_blueprint
-import subprocess
-
-subprocess.Popen([
-    "cmd", "/c", "start",
-    "core/caddy", "reverse-proxy",
-    "--from", "aiassistant.ddnsking.com",
-    "--to", "localhost:5000"
-])
 
 app = Flask(__name__)
 app.register_blueprint(chat_blueprint)
@@ -18,3 +10,4 @@ def index():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
+
